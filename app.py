6950208cb520cada668e4e6767dc86fe4946ab72
osama-bitk <osama.kk.2017@gmail.com>
2021-07-28 22:28:52 -0230

    Added new route

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-print(__name__)
-
 @app.route("/")
 def hello_world():
-    return render_template("hello.html")
+    return "<p>Hello, World!</p>"
+
+@app.route("/<name>")
+def hello_name(name):
+    return f"<p>Hello, {name}!</p>"
